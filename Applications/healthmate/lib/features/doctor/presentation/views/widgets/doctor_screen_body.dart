c5d2a3c6da5f3be03200ea34408ac27d375c82fc
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-28 22:25:04 +0200

    finish doctor info screen

--- Applications/healthmate/lib/features/doctor/presentation/views/widgets/doctor_screen_body.dart
+++ Applications/healthmate/lib/features/doctor/presentation/views/widgets/doctor_screen_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:healthmate/core/utils/color_style.dart';
+import 'package:healthmate/core/utils/style.dart';
 import 'package:healthmate/core/widgets/custom_app_bar.dart';
+import 'package:healthmate/core/widgets/custom_button.dart';
 import 'package:healthmate/features/doctor/presentation/views/widgets/card_information.dart';
 import 'package:healthmate/features/doctor/presentation/views/widgets/custom_list_tiel_doctor.dart';
 import 'package:healthmate/features/doctor/presentation/views/widgets/row_card_information.dart';
@@ -12,6 +15,7 @@ class DoctorScreenBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
             height: 30,
@@ -27,6 +31,107 @@ class DoctorScreenBody extends StatelessWidget {
           RowCardInformation(),
           SizedBox(
             height: 21,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              'About',
+              style: StylingSystem.textStyle20semibold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: SizedBox(
+              width: 362,
+              height: 96,
+              child: Text(
+                'Lorem ipsum dolor sit amet consectetur. Sed integer am congue enim libero urna odio. In accumsan odio mauris nibh. Et elementum enim at enim montes aliquam elit pellentesque nulla.',
+                style: StylingSystem.textStyle16Medium.copyWith(
+                  color: ColorSystem.kGrayColor2,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              'Working hours',
+              style: StylingSystem.textStyle20semibold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Monday',
+                  style: StylingSystem.textStyle16Medium.copyWith(
+                    color: ColorSystem.kGrayColor2,
+                  ),
+                ),
+                Text(
+                  '05:00 - 15:00',
+                  style: StylingSystem.textStyle16Medium.copyWith(
+                    color: ColorSystem.kGrayColor2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Monday',
+                  style: StylingSystem.textStyle16Medium.copyWith(
+                    color: ColorSystem.kGrayColor2,
+                  ),
+                ),
+                Text(
+                  '05:00 - 15:00',
+                  style: StylingSystem.textStyle16Medium.copyWith(
+                    color: ColorSystem.kGrayColor2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              'consulatiion fee',
+              style: StylingSystem.textStyle20semibold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              '1500 EGP',
+              style: StylingSystem.textStyle16Medium.copyWith(
+                color: ColorSystem.kGrayColor2,
+              ),
+            ),
+          ),
+          Spacer(),
+          CustomButton(
+            onPressed: () {},
+            text: 'Make Appointment',
+            width: 362,
+            height: 48,
+            textColor: Colors.white,
+            backgrounColor: ColorSystem.kPrimaryColor,
+          ),
+          SizedBox(
+            height: 24,
           ),
         ],
       ),
